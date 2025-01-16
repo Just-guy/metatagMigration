bfc1ffd386b698290f5ffc3118ece5634c6092a2
Just-guy <62421873+Just-guy@users.noreply.github.com>
2025-01-16 09:58:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+require_once($_SERVER["DOCUMENT_ROOT"] . "section.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "element.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "helper.php");
+
+// В качестве аргумента для конструктора необходимо указать ID инфоблока
+//new metatagMigration\Section('15');
+//metatagMigration\Section::export();
+//metatagMigration\Section::import();
+
+//new metatagMigration\Element('15');
+//metatagMigration\Element::export();
+//metatagMigration\Element::import();
